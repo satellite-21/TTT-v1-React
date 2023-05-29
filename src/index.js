4ceaa6d721e408a5f497e555953e3b8a1dd9ff52
satellite-21 <www.kartiksuccess832@gmail.com>
2023-05-29 14:41:48 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//this file will link the HTML file and the react code 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+//this is the initialization process
+ReactDOM.render(
+
+    //this takes two parameters , a component to render
+    //and the target DOM element
+
+    //the <React.StrictMode> component performs 
+    //additional runtime checks and warnings 
+    //used as a wrapper around the <App/> component
+    <React.StrictMode> 
+        <App />
+    </React.StrictMode>,
+
+    //the <App/> component refers to custom
+    //react component is the root of the applicationn
+    //and handles the components and logics 
+
+    document.getElementById('root') 
+    //this is the targetDOM element
+    //this retrieves the DOM element with 
+    //the ID root where the react component
+    //will be rendered 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
